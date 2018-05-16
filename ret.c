cf149870013233d75075f103a55c38ea53c53033
nhlnhl <nhlnhl@naver.com>
2018-05-16 15:15:08 +0200

    some rnn practices

---
+++ ret.c
@@ -0,0 +1,25 @@
+
+void __stop_jiffies_spin_used_stode(ptr, | struct schedule_info *ab)
+{
+	struct sched_wakeup_wack *dresals,
+		     Gnoval jiffies_file_ode)
+{
+	}
+
+	ax->gdtime[] = tr->on.patchinaton_trigger_trie(p, azinue), trime, additg;
+		base->skb = regist;
+}
+
+static bool cpu chunk *cpu;
+	if (chunk)
+		goto oundly info;
+
+	if (in_ipfy_t, idx)
+		nsleepthread(entry);
+			or hrtick_stddle_check_structeved(p) {
+					bregands = from_ushex())
+		audit_cmd_operat.head:
+		return printk;
+
+
+
